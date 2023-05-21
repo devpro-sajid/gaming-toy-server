5d67bf7913f70317747566e7699a35ebdb3df345
devpro-sajid <sfsajidforclash01@gmail.com>
2023-05-21 12:42:42 +0600

    dbcollection created

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const db = client.db("toysDB");
+        const toysCollection = db.collection("toys");
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
