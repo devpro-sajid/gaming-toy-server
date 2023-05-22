c4423469a7f1974a7d672ab40f508f7caa42a6f3
devpro-sajid <sfsajidforclash01@gmail.com>
2023-05-22 20:47:53 +0600

    get By cat done

--- index.js
+++ index.js
@@ -26,9 +26,49 @@ async function run() {
 
         const db = client.db("toysDB");
         const toysCollection = db.collection("toys");
+
+
+        const indexKeys = { toyName: 1 }; // Replace field1 and field2 with your actual field names
+        const indexOptions = { name: "toyName" }; // Replace index_name with the desired index name
+        const result = await toysCollection.createIndex(indexKeys, indexOptions);
+        console.log(result);
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+        app.get("/getToysByName/:text", async (req, res) => {
+            const text = req.params.text;
+            const result = await toysCollection
+                .find(
+                    { toyName: { $regex: text, $options: "i" } }
+                ).toArray().limit(20);
+            res.send(result);
+        });
+
+        app.get("/allToysByCategory/:cat", async (req, res) => {
+            console.log(req.params.category);
+            const toys = await toysCollection
+              .find({
+                category: req.params.cat
+              })
+              .toArray();
+            res.send(toys);
+          });
+
+        app.post("/add-toy", async (req, res) => {
+            const body = req.body;
+            console.log(body);
+            const result = await toysCollection.insertOne(body);
+            if (result?.insertedId) {
+              return res.status(200).send(result);
+            } else {
+              return res.status(404).send({
+                message: "can not insert try again leter",
+                status: false,
+              });
+            }
+          });
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
